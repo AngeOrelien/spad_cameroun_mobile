187c79ebb433121149321540ef368f3bc69b2c80
AngeOrelien <fossoorelien@gmail.com>
2026-05-01 01:06:36 +0100

    initial commit: initializing project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:spad_cameroun/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(SpadCameroun());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
